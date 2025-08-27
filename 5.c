558c4f8cc1b78e16a32478c89240f67a5ca91304
DIVYANSH GUPTA <divyanshg221220@outlook.com>
2025-08-27 06:46:37 +0000

    .

---
+++ 5.c
@@ -0,0 +1,19 @@
+// Find Simple and Compound Interest
+//     SI=(P * R * T)/100
+//     CI=P*pow((1+R/100),T)-P
+#include <stdio.h>
+#include <math.h>
+int main() {
+    float p,r,t,si,ci;
+    printf("Enter Principal Amount : ");
+    scanf("%f",&p);
+    printf("Enter Rate of Interest : ");
+    scanf("%f",&r);
+    printf("Enter Time in Years : ");
+    scanf("%f",&t);
+    si=(p*r*t)/100;
+    ci=p*pow((1+r/100),t)-p;
+    printf("Simple Interest = %.2f\n",si);
+    printf("Compound Interest = %.2f",ci);
+    return 0;
+}
