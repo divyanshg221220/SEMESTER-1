36e9802b9f7ad6e1a7b2826596e0fb10e150e0d9
divyanshg221220 <divyanshg221220@outlook.com>
2025-10-06 00:38:33 +0530

    .

--- PYTHON PRACTICAL/27.py
+++ PYTHON PRACTICAL/27.py
@@ -1,12 +1,14 @@
 # Using NumPy, write a program to create Multi-Dim Array, load it with the numbers and display the content of it. 
 import numpy as np
+shape=[]
+dim=int(input("Enter number of dimensions: "))
+total_elements=1
+for i in range(dim):
+    n=int(input(f"Enter size for dimension {i+1}: "))
+    shape.append(n)
+    total_elements*=n
 l=[]
-row=int(input("Enter number of rows: "))
-column=int(input("Enter number of columns: "))
-for i in range(row):
-    temp=[]
-    for j in range(column):
-        temp.append(int(input("Enter the number: ")))
-    l.append(temp)
-arr=np.array(l)
-print("Array:",arr)
+for i in range(total_elements):
+    l.append(eval(input("Enter the element: ")))
+arr=np.array(l).reshape(shape)
+print("Array:\n",arr)
