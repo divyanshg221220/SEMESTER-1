ff2347aa500bca1e2e15e826bb3e4235f4a1d701
divyanshg221220 <divyanshg221220@outlook.com>
2025-10-14 23:48:05 +0530

    .

--- C PRACTICAL/20.c
+++ C PRACTICAL/20.c
@@ -1,21 +1,17 @@
 // Write a program to input 5 numbers into an array and print their sum.
-// Write a program to input a 2×2 matrix and display its transpose.
-// Write a program in c to read n number of values in an array and display them in reverse order
-// Write a C program to print the square of array element
-// Write a C program to find sum of array elements using Pointer as Arguments
 #include <stdio.h>
 int main(int argc, char const *argv[])
 {
-    int arr[5],sum=0;
+    int array[5],sum=0;
     for (int i=0;i<5;i++)
     {
-        printf("Enter number: ");
-        scanf("%d",&arr[i]);
+        printf("Enter [%d] element: ",i);
+        scanf("%d",&array[i]);
     }
     for (int i=0;i<5;i++)
     {
-        sum+=arr[i];
+        sum+=array[i];
     }
-    printf("Sum: %d",&sum);
+    printf("Sum of array elements: %d",sum);
     return 0;
 }
