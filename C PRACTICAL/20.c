815adba8069433e83e8a00087c182ca10305f797
DIVYANSH GUPTA <divyanshg221220@outlook.com>
2025-10-14 09:27:28 +0530

    Create 20.c

---
+++ C PRACTICAL/20.c
@@ -0,0 +1,11 @@
+Understanding Arrays and pointers
+
+Q1. Write a program to input 5 numbers into an array and print their sum.
+
+Q2. Write a program to input a 2×2 matrix and display its transpose.
+
+Q3.Write a program in c to read n number of values in an array and display them in reverse order
+
+Q4.Write a C program to print the square of array element
+
+Q5.Write a C program to find sum of array elements using Pointer as Arguments
