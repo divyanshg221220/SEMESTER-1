1dcc49f653194079c93f2242c43e23721de6cd23
DIVYANSH GUPTA <divyanshg221220@outlook.com>
2025-10-14 15:56:25 +0000

    .

--- C PRACTICAL/20.c
+++ C PRACTICAL/20.c
@@ -1,11 +1,21 @@
-Understanding Arrays and pointers
-
-Q1. Write a program to input 5 numbers into an array and print their sum.
-
-Q2. Write a program to input a 2×2 matrix and display its transpose.
-
-Q3.Write a program in c to read n number of values in an array and display them in reverse order
-
-Q4.Write a C program to print the square of array element
-
-Q5.Write a C program to find sum of array elements using Pointer as Arguments
+// Write a program to input 5 numbers into an array and print their sum.
+// Write a program to input a 2×2 matrix and display its transpose.
+// Write a program in c to read n number of values in an array and display them in reverse order
+// Write a C program to print the square of array element
+// Write a C program to find sum of array elements using Pointer as Arguments
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    int arr[5],sum=0;
+    for (int i=0;i<5;i++)
+    {
+        printf("Enter number: ");
+        scanf("%d",&arr[i]);
+    }
+    for (int i=0;i<5;i++)
+    {
+        sum+=arr[i];
+    }
+    printf("Sum: %d",&sum);
+    return 0;
+}
