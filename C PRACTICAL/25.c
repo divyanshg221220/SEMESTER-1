8f0287b44d744eeca097be7b7e4a38e367dfd7b9
divyanshg221220 <divyanshg221220@outlook.com>
2025-10-21 14:02:45 +0530

    .

--- C PRACTICAL/25.c
+++ C PRACTICAL/25.c
@@ -9,7 +9,8 @@ int main(int argc, char const *argv[])
     isPrime(n)?printf("%d is a prime number\n",n):printf("%d is not a prime number\n",n);
     return 0;
 }
-int isPrime(int n) {
+int isPrime(int n)
+{
     if (n<=1) return 0;
     for (int i=2;i<n;i++) if (n%i==0) return 0;
     return 1;
