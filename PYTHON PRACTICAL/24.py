3e4bad455b4906a528389364170641cf3dfe3cd0
divyanshg221220 <divyanshg221220@outlook.com>
2025-11-06 22:00:20 +0530

    .

--- PYTHON PRACTICAL/24.py
+++ PYTHON PRACTICAL/24.py
@@ -1,7 +1,7 @@
 # Using NumPy, write a program to create two 1 Dim Array and perform the operation of Iteration, Sorting the contents of array and concatenating the contents of the array.
 import numpy as np
-a = np.array([int(x) for x in input("Enter elements of first array separated by space: ").split()])
-b = np.array([int(x) for x in input("Enter elements of second array separated by space: ").split()])
+a=np.array([int(x) for x in input("Enter elements of first array separated by space: ").split()])
+b=np.array([int(x) for x in input("Enter elements of second array separated by space: ").split()])
 print("First array elements:")
 for i in a:
     print(i)
@@ -10,7 +10,7 @@ for i in b:
     print(i)
 a.sort()
 b.sort()
-print("Sorted first array:", a)
-print("Sorted second array:", b)
-c = np.concatenate((a, b))
-print("Concatenated array:", c)
+print("Sorted first array:",a)
+print("Sorted second array:",b)
+c = np.concatenate((a,b))
+print("Concatenated array:",c)
