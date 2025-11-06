0580061b66375571daac0a627cac3ce9dee555c3
divyanshg221220 <divyanshg221220@outlook.com>
2025-11-06 22:03:30 +0530

    .

--- PYTHON PRACTICAL/25.py
+++ PYTHON PRACTICAL/25.py
@@ -1,5 +1,5 @@
 # Using NumPy, initialize the array and display their dimensionality.
 import numpy as np
 arr=np.array([int(x) for x in input("Enter elements of the array separated by space: ").split()])
-print("Array:",arr)
-print("Dimensionality of the array:",arr.ndim)
+print("Array: ",arr)
+print("Dimensionality of the array: ",arr.ndim)
