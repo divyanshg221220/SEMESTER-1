aaf67253466b5f26c9a4827bb9bee22fced665f7
divyanshg221220 <divyanshg221220@outlook.com>
2025-11-09 17:15:42 +0530

    .

--- PYTHON PRACTICAL/25.py
+++ PYTHON PRACTICAL/25.py
@@ -1,5 +1,15 @@
 # Using NumPy, initialize the array and display their dimensionality.
 import numpy as np
-arr=np.array([int(x) for x in input("Enter elements of the array separated by space: ").split()])
-print("Array: ",arr)
-print("Dimensionality of the array: ",arr.ndim)
+shape=[]
+dim=int(input("Enter number of dimensions: "))
+total_elements=1
+for i in range(dim):
+    n=int(input(f"Enter size for dimension {i+1}: "))
+    shape.append(n)
+    total_elements*=n
+l=[]
+for i in range(total_elements):
+    l.append(eval(input("Enter the element: ")))
+arr=np.array(l).reshape(shape)
+print("Array:\n",arr)
+print("Dimensionality of the array:",arr.ndim)
