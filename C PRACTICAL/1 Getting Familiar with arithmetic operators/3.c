c7aa5fc19d684ad0cafd76d43e3cdfd66bdb8b8b
DIVYANSH GUPTA <divyanshg221220@outlook.com>
2025-11-28 04:20:50 +0000

    .

--- C PRACTICAL/1 Getting Familiar with arithmetic operators/3.c
+++ C PRACTICAL/1 Getting Familiar with arithmetic operators/3.c
@@ -4,12 +4,13 @@
 #include <stdio.h>
 int main(int argc, char const *argv[])
 {
-    float q,c,f;
+    int q;
+    float c,f;
     printf("Convert Celsius to Fahrenheit and Fahrenheit to Celsius (take value till 2 decimal places)\n");
     printf("1. Convert Celsius to Fahrenheit\n");
     printf("2. Convert Fahrenheit to Celsius\n");
     printf("Enter your choice : ");
-    scanf("%f",&q);
+    scanf("%d",&q);
     if (q==1)
     {
         printf("Enter Temperature in Celsius : ");
