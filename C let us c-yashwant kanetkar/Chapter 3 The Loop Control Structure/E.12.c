966c3aa3367313b9bbd0911d5e076c639bc8df15
divyanshg221220 <divyanshg221220@outlook.com>
2025-12-15 16:28:20 +0530

    e

---
+++ C let us c-yashwant kanetkar/Chapter 3 The Loop Control Structure/E.12.c
@@ -0,0 +1,26 @@
+// When interest compounds q times per year at an annual rate of r % for n years, the principle p compounds to an amount a as per the following formula
+// a = p ( 1 + r / q ) ^ nq
+// Write a program to read 10 sets of p, r, n & q and calculate the corresponding as.
+#include <stdio.h>
+#include <math.h>
+int main(int argc, char const *argv[])
+{
+    float p[10], r[10], n[10], q[10], a[10];
+    for (int i = 0; i < 10; i++)
+    {
+        printf("Enter principal amount %d: ", i + 1);
+        scanf("%f", &p[i]);
+        printf("Enter annual interest rate %d: ", i + 1);
+        scanf("%f", &r[i]);
+        printf("Enter number of years %d: ", i + 1);
+        scanf("%f", &n[i]);
+        printf("Enter number of times interest compounds per year %d: ", i + 1);
+        scanf("%f", &q[i]);
+    }
+    for (int i = 0; i < 10; i++)
+    {
+        a[i] = p[i] * pow(1 + ( r[i] / q[i] ), n[i] * q[i]);
+        printf("amount %d: %.2f\n", i + 1, a[i]);
+    }
+    return 0;
+}
---
+++ C let us c-yashwant kanetkar/Chapter 3 The Loop Control Structure/E.13.c
@@ -0,0 +1,19 @@
+// The natural logarithm can be approximated by the following series.
+// (x - 1) / 2 + ( 1 / 2 ) * ( (x - 1) / x ) ^ 2 + ( 1 / 2 ) * ( (x - 1) / x ) ^ 3 + ( 1 / 2 ) * ( (x - 1) / x ) ^ 4 + ....
+// If x is input through the keyboard, write a program to calculate the sum of first seven terms of this series.
+#include <stdio.h>
+#include <math.h>
+int main(int argc, char const *argv[])
+{
+    float x, sum = 0.0, term;
+    printf("Enter the value of x: ");
+    scanf("%f", &x);
+    sum += ( x - 1.0 ) / 2.0;
+    for (int i = 2.0; i <= 7.0; i++)
+    {
+        term = ( 1.0 / 2.0 ) * pow( (x - 1.0) / x, i);
+        sum += term;
+    }
+    printf("sum: %.2f\n", sum);
+    return 0;
+}
