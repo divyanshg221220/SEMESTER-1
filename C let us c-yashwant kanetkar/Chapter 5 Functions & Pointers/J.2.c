63215725e0b77d4bec3c7694ab0e405a394d6869
divyanshg221220 <divyanshg221220@outlook.com>
2025-12-17 00:06:19 +0530

    added:   C let us c-yashwant kanetkar/Chapter 5 Functions & Pointers/J.3.c fix: corrected function implementation in J.2.c

--- C let us c-yashwant kanetkar/Chapter 5 Functions & Pointers/J.2.c
+++ C let us c-yashwant kanetkar/Chapter 5 Functions & Pointers/J.2.c
@@ -1,24 +1,19 @@
 // A positive integer is entered through the keyboard, write a program to obtain the prime factors of the number. Modify the function suitably to obtain the prime factors recursively.
 #include <stdio.h>
-void prime_factors(int num)
+void prime_factors(int num, int i)
 {
-    for (int i = 2; i <= num; i++)
-    {  
-        int isPrime = 1;
-        for (int j = 2; j * j <= i; j++)
-        {
-            if (i % j == 0)
-            {
-                isPrime = 0;
-                break;
-            }
-        }
-        if (!isPrime)
-            continue;
-        if (num % i == 0)
-        {
-            printf("%d\n", i);
-        }
+    if (num <= 1)
+    {
+        return;
+    }
+    if (num % i == 0)
+    {
+        printf("%d ", i);
+        prime_factors(num / i, i);
+    }
+    else
+    {
+        prime_factors(num, i + 1);
     }
 }
 int main(int argc, char const *argv[])
@@ -27,6 +22,6 @@ int main(int argc, char const *argv[])
     printf("Enter a positive integer: ");
     scanf("%d", &num);
     printf("prime factors: ");
-    prime_factors(num);
+    prime_factors(num, 2);
     return 0;
 }
