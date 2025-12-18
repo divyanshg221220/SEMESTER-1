b5cae6fb1e3632d0cf6d4ab108582a2c477942d9
divyanshg221220 <divyanshg221220@outlook.com>
2025-12-18 18:46:07 +0530

    .

--- C let us c-yashwant kanetkar/Chapter 8 Arrays/I.5.c
+++ C let us c-yashwant kanetkar/Chapter 8 Arrays/I.5.c
@@ -2,27 +2,42 @@
 // For example, the ASCII value of the character present on zeroth row and zeroth column on the screen is stored at location number 0xB8000000. Therefore the colour of this character would be present at location number 0xB8000000 + 1. Similarly ASCII value of character in row 0, col 1 will be at location 0xB8000000 + 2, and its colour at 0xB8000000 + 3. 
 // With this knowledge write a program which when executed would keep converting every capital letter on the screen to small case letter and every small case letter to capital letter. The procedure should stop the moment the user hits a key from the keyboard. 
 // This is an activity of a rampant Virus called Dancing Dolls. (For monochrome adapter, use 0xB0000000 instead of 0xB8000000). 
-#include <dos.h>
+#include <windows.h>
 #include <conio.h>
-void main()
+int main(int argc, char const *argv[])
 {
-    char far *vdu_mem;
-    int i;
-    vdu_mem = (char far *)0xB8000000L; 
+    HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_SCREEN_BUFFER_INFO csbi;
+    if (!GetConsoleScreenBufferInfo(h, &csbi))
+    {
+        return 1;
+    }
+    int width = 25, height = 80;
+    DWORD read;
     while (!kbhit())
     {
-        for (i = 0; i < 4000; i += 2)
+        for (int y = 0; y < height; ++y)
         {
-            char current_char = vdu_mem[i];
-            if (current_char >= 'A' && current_char <= 'Z')
+            for (int x = 0; x < width; ++x)
             {
-                vdu_mem[i] = current_char + 32;
-            }
-            else if (current_char >= 'a' && current_char <= 'z')
-            {
-                vdu_mem[i] = current_char - 32;
+                COORD pos = { (SHORT)x, (SHORT)y };
+                char ch = 0;
+                if (!ReadConsoleOutputCharacterA(h, &ch, 1, pos, &read))
+                {
+                    continue;
+                }
+                if (ch >= 'A' && ch <= 'Z')
+                {
+                    ch = ch + 32;
+                }
+                else if (ch >= 'a' && ch <= 'z')
+                {
+                    ch = ch - 32;
+                }
+                WriteConsoleOutputCharacterA(h, &ch, 1, pos, &read);
             }
         }
+        Sleep(50);
     }
-    getch(); 
+    return 0;
 }
