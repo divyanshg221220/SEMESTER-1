4f40e59703bf80e547858fe8dc57f54d5a9b578d
divyanshg221220 <divyanshg221220@outlook.com>
2026-01-08 14:50:41 +0530

    fix: some bug while writing the file name and gitignore updation

--- C let us c-yashwant kanetkar/Chapter 12 File Input Output/C.1.c
+++ C let us c-yashwant kanetkar/Chapter 12 File Input Output/C.1.c
@@ -4,20 +4,33 @@
 int main(int argc, char const *argv[])
 {
     FILE *fptr;
-    char fname;
+    char fname[10];
     printf("Enter file name: ");
     scanf("%s", &fname);
-    fptr = fopen(&fname, "r");
+    fptr = fopen(fname, "r");
     if (fptr == NULL)
     {
         printf("error opening file");
     }
     else
     {
+        int i = 1;
         char c;
         while ((c = fgetc(fptr)) != EOF)
         {
-            printf("%c", c);
+            if (i == 1 || c == '\n')
+            {
+                if (c == '\n')
+                {
+                    printf("\n");
+                }
+                printf("%d. ", i);
+                i++;
+            }
+            else
+            {
+                printf("%c", c);
+            }
         }
         fclose(fptr);
     }
