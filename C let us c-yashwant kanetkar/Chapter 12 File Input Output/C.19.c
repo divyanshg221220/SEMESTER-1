4a3b1a172865e25b80cdb6b8cb7b97a7eaac2427
divyanshg221220 <divyanshg221220@outlook.com>
2026-01-13 15:57:09 +0530

    feat: // Write a program to carry out the following: // (a) Read a text file ‘INPUT.TXT’ // (b) Print each word in reverse order // Example, // Input: INDIA IS MY COUNTRY // Output: AIDNI SI YM YRTNUOC // Assume that each word length is maximum of 10 characters and each word is separated by newline/blank characters.

---
+++ C let us c-yashwant kanetkar/Chapter 12 File Input Output/C.19.c
@@ -0,0 +1,39 @@
+// Write a program to carry out the following: 
+// (a) Read a text file ‘INPUT.TXT’ 
+// (b) Print each word in reverse order 
+// Example, 
+// Input: INDIA IS MY COUNTRY 
+// Output: AIDNI SI YM YRTNUOC 
+// Assume that each word length is maximum of 10 characters and each word is separated by newline/blank characters. 
+#include <stdio.h>
+#include <string.h>
+int main(int argc, char const *argv[])
+{
+    FILE *fptr;
+    fptr = fopen("INPUT.TXT", "r");
+    if (fptr == NULL)
+    {
+        printf("error opening file");
+    }
+    else
+    {
+        int i = 0;
+        char c[10][10], *word;
+        while (fgets(c[i], sizeof(c), fptr) != NULL)
+        {
+            word = strtok(c[i], " \n");
+            while (word != NULL)
+            {
+                printf("%s", strrev(word));
+                word = strtok(NULL, " \n");
+                if (word != NULL)
+                {
+                    printf(" ");
+                }
+            }
+            i++;
+        }
+        fclose(fptr);
+    }
+    return 0;
+}
