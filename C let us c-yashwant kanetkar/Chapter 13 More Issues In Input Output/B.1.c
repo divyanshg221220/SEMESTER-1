f215a152c773e57e4491eedd5e44e67ab877b5fa
divyanshg221220 <divyanshg221220@outlook.com>
2026-01-13 16:36:53 +0530

    feat: // Write a program to carry out the following: // (a) Read a text file provided at command prompt // (b) Print each word in reverse order // For example if the file contains // INDIA IS MY COUNTRY // Output should be // AIDNI SI YM YRTNUOC

---
+++ C let us c-yashwant kanetkar/Chapter 13 More Issues In Input Output/B.1.c
@@ -0,0 +1,42 @@
+// Write a program to carry out the following: 
+// (a) Read a text file provided at command prompt 
+// (b) Print each word in reverse order 
+// For example if the file contains 
+// INDIA IS MY COUNTRY 
+// Output should be
+// AIDNI SI YM YRTNUOC
+#include <stdio.h>
+#include <string.h>
+int main(int argc, char const *argv[])
+{
+    FILE *fptr;
+    char fname[10];
+    printf("Enter file name: ");
+    scanf("%s", fname);
+    fptr = fopen(fname, "r");
+    if (fptr == NULL)
+    {
+        printf("error opening file");
+    }
+    else
+    {
+        int i = 0;
+        char c[50][50], *word;
+        while (fgets(c[i], sizeof(c), fptr) != NULL)
+        {
+            word = strtok(c[i], " \n");
+            while (word != NULL)
+            {
+                printf("%s", strrev(word));
+                word = strtok(NULL, " \n");
+                if (word != NULL)
+                {
+                    printf(" ");
+                }
+            }
+            i++;
+        }
+        fclose(fptr);
+    }
+    return 0;
+}
