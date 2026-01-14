230ec44f0d330e5744d46014af792cd90fe018d5
divyanshg221220 <divyanshg221220@outlook.com>
2026-01-14 23:27:06 +0530

    fix: removing unwanted library from the library call

--- C let us c-yashwant kanetkar/Chapter 16  C Under Windows/C.2.c
+++ C let us c-yashwant kanetkar/Chapter 16  C Under Windows/C.2.c
@@ -1,6 +1,5 @@
 // Write a program that displays three buttons ‘Yes’, ‘No’ ‘Cancel’ in the message box. 
 #include <windows.h>
-#include <stdio.h>
 int _stdcall WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
     MessageBox(0, NULL, NULL, MB_YESNOCANCEL);
