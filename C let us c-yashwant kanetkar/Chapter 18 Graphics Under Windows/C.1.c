00393af9af86a8edb55a06511dcc44185fe9493a
divyanshg221220 <divyanshg221220@outlook.com>
2026-01-17 14:45:47 +0530

    refactor: restructure mouse click handling and painting logic

--- C let us c-yashwant kanetkar/Chapter 18 Graphics Under Windows/C.1.c
+++ C let us c-yashwant kanetkar/Chapter 18 Graphics Under Windows/C.1.c
@@ -1,62 +1,93 @@
 // Write a program, which displays "hello" at any place in the window where you click the left mouse button. If you click the right mouse button the color of subsequent hellos should change. 
 #include <windows.h>
-#include <stdio.h>
-static COLORREF g_textColor = RGB(0, 0, 0); 
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
-    switch (uMsg) {
-        case WM_LBUTTONDOWN: {
-            int x = LOWORD(lParam);
-            int y = HIWORD(lParam);
-            HDC hdc = GetDC(hwnd);
-            SetTextColor(hdc, g_textColor);
-            SetBkMode(hdc, TRANSPARENT);
-            TextOutA(hdc, x, y, "hello", 5);
-            ReleaseDC(hwnd, hdc);
-            return 0;
-        }
-        case WM_RBUTTONDOWN: {
-            // Cycle through colors: black -> red -> green -> blue -> black
-            if (g_textColor == RGB(0,0,0)) g_textColor = RGB(255,0,0);
-            else if (g_textColor == RGB(255,0,0)) g_textColor = RGB(0,255,0);
-            else if (g_textColor == RGB(0,255,0)) g_textColor = RGB(0,0,255);
-            else g_textColor = RGB(0,0,0);
-            return 0;
-        }
-        case WM_DESTROY:
-            PostQuitMessage(0);
-            return 0;
-    }
-    return DefWindowProcA(hwnd, uMsg, wParam, lParam);
-}
-
-int __stdcall WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+#include "helper.h"
+#define max_click 100
+void OnLeftClick(HWND, LPARAM);
+void OnRightClick();
+void OnDestroy(HWND);
+void OnPaint(HWND);
+static COLORREF textColor = RGB(0, 0, 0);
+static COLORREF backgroundColor = RGB(255, 255, 255);
+struct click_data
 {
-    const char CLASS_NAME[] = "HelloClickClass";
-
-    WNDCLASSA wc = {0};
-    wc.lpfnWndProc = WindowProc;
-    wc.hInstance = hInstance;
-    wc.lpszClassName = CLASS_NAME;
-    wc.hbrBackground = (HBRUSH)(COLOR_WINDOW+1);
-    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
-    RegisterClassA(&wc);
-
-    HWND hwnd = CreateWindowExA(
-        0,
-        CLASS_NAME,
-        "Click to Hello",
-        WS_OVERLAPPEDWINDOW | WS_VISIBLE,
-        CW_USEDEFAULT, CW_USEDEFAULT, 800, 600,
-        NULL, NULL, hInstance, NULL
-    );
-
-    if (hwnd == NULL) return 0;
-
-    MSG msg = {0};
-    while (GetMessageA(&msg, NULL, 0, 0)) {
-        TranslateMessage(&msg);
-        DispatchMessageA(&msg);
+    int x;
+    int y;
+    COLORREF color;
+} clicks[max_click];
+static int click_count = 0; 
+int __stdcall WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int nCmdShow)
+{
+    MSG m;
+    InitInstance(hInstance, nCmdShow, "hello");
+    while (GetMessage(&m, NULL, 0, 0))
+    {
+        TranslateMessage(&m);
+        DispatchMessage(&m);
     }
-
     return 0;
+}
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+        case WM_LBUTTONDOWN:
+            OnLeftClick(hWnd, lParam);
+            break;
+        case WM_RBUTTONDOWN:
+            OnRightClick();
+            break;
+        case WM_DESTROY:
+            OnDestroy(hWnd);
+            break;
+        case WM_PAINT:
+            OnPaint(hWnd);
+            break;
+        default:
+            return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+void OnLeftClick(HWND hWnd, LPARAM lParam)
+{
+    if (click_count < max_click)
+    {
+        clicks[click_count].x = LOWORD(lParam);
+        clicks[click_count].y = HIWORD(lParam);
+        clicks[click_count].color = textColor;
+        click_count++;
+        InvalidateRect(hWnd, NULL, TRUE);
+    }
+}
+void OnRightClick()
+{
+    if (textColor == RGB(0,0,0)) textColor = RGB(255,0,0);
+    else if (textColor == RGB(255, 0, 0)) textColor = RGB(0, 255, 0);
+    else if (textColor == RGB(0, 255, 0)) textColor = RGB(0, 0, 255);
+    else if (textColor == RGB(0, 0, 255)) textColor = RGB(0, 0, 0);
+}
+void OnDestroy(HWND hWnd)
+{
+    PostQuitMessage(0);
+}
+void OnPaint(HWND hWnd)
+{
+    HDC hdc;
+    PAINTSTRUCT ps;
+    HFONT hfont;
+    LOGFONT f = {0};
+    HGDIOBJ holdfont;
+    char *font[] = { "Arial", "Times New Roman", "Comic Sans MS" } ;
+    int i;
+    hdc = BeginPaint(hWnd, &ps);
+    SetBkMode(hdc, backgroundColor);
+    for (i = 0; i < click_count; i++)
+    {
+        hfont = CreateFontIndirect(&f);
+        holdfont = SelectObject(hdc, hfont);
+        SetTextColor(hdc, clicks[i].color);
+        TextOut(hdc, clicks[i].x, clicks[i].y, "hello", 5);
+        SelectObject(hdc, holdfont);
+        DeleteObject(hfont);
+    }
+    EndPaint(hWnd, &ps);
 }
