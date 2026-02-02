0ce63e01934ccde3fce05f21a166de045e7a4314
divyanshg221220 <divyanshg221220@outlook.com>
2026-02-03 01:19:28 +0530

    feat: .

---
+++ C let us c-yashwant kanetkar/Chapter 9 Puppetting On Strings/F.6.c
@@ -0,0 +1,37 @@
+// A factory has 3 division and stocks 4 categories of products. An inventory table is updated for each division and for each product as they are received. There are three independent suppliers of products to the factory: 
+// (a) Design a data format to represent each transaction. 
+// (b) Write a program to take a transaction and update the inventory. 
+// (c) If the cost per item is also given write a program to calculate the total inventory values. 
+#include <stdio.h>
+struct product
+{
+    int division;
+    int category;
+    int supplier;
+    float cost;
+};
+int main(int argc, char const *argv[])
+{
+    int n;
+    printf("Enter number of transactions: ");
+    scanf("%d", &n);
+    struct product inventory[n];
+    for (int i = 0; i < n; i++)
+    {
+        printf("Enter division: ");
+        scanf("%d", &inventory[i].division);
+        printf("Enter category: ");
+        scanf("%d", &inventory[i].category);
+        printf("Enter supplier: ");
+        scanf("%d", &inventory[i].supplier);
+        printf("Enter cost: ");
+        scanf("%f", &inventory[i].cost);
+    }
+    float totalvalue = 0;
+    for (int i = 0; i < n; i++)
+    {
+        totalvalue += inventory[i].cost;
+    }
+    printf("total inventory value: %.2f\n", totalvalue);
+    return 0;
+}
