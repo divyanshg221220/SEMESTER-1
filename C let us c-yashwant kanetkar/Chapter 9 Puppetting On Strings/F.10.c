c2015d27a4bd03cf24603b37e611d61fff47cd40
DIVYANSH GUPTA <divyanshg221220@outlook.com>
2026-02-13 05:33:14 +0000

    feat: Write a program that takes a set of names of individuals and abbreviates the first, middle and other names except the last name by their first letter. #include <stdio.h>

---
+++ C let us c-yashwant kanetkar/Chapter 9 Puppetting On Strings/F.10.c
@@ -0,0 +1,33 @@
+// Write a program that takes a set of names of individuals and abbreviates the first, middle and other names except the last name by their first letter.
+#include <stdio.h>
+#include <string.h>
+int main(int argc, char const *argv[])
+{
+    char name[4][25];
+    for (int i = 0; i < 4; i++)
+    {
+        if (i == 0)
+        {
+            printf("Enter first name: ");
+        }
+        else if (i == 1)
+        {
+            printf("Enter middle name: ");
+        }
+        else if (i == 2)
+        {
+            printf("Enter other name: ");
+        }
+        else
+        {
+            printf("Enter last name: ");
+        }
+        scanf("%s", name[i]);
+    }
+    for (int i = 0; i < 3; i++)
+    {
+        printf("%c. ", name[i][0]);
+    }
+    printf("%s", name[3]);
+    return 0;
+}
