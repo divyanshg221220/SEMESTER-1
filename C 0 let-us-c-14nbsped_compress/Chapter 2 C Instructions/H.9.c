7e272957d0c6a0b837db0dc3a1b372b6757fd08e
DIVYANSH GUPTA <divyanshg221220@outlook.com>
2026-02-17 10:00:44 +0000

    feat: Consider a currency system in which there are notes of seven denominations, namely, Re. 1, Rs. 2, Rs. 5, Rs. 10, Rs. 50, Rs. 100. If a sum of Rs. N is entered through the keyboard, write a program to compute the smallest number of notes that will combine to give Rs. N.

---
+++ C 0 let-us-c-14nbsped_compress/Chapter 2 C Instructions/H.9.c
@@ -0,0 +1,18 @@
+// Consider a currency system in which there are notes of seven denominations, namely, Re. 1, Rs. 2, Rs. 5, Rs. 10, Rs. 50, Rs. 100. If a sum of Rs. N is entered through the keyboard, write a program to compute the smallest number of notes that will combine to give Rs. N.
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    int currency[6] = {100, 50, 10, 5, 2, 1}, N, count = 0;
+    printf("Enter the amount: ");
+    scanf("%d", &N);
+    for (int i = 0; i < 6; i++)
+    {
+        if (N >= currency[i])
+        {
+            count += N / currency[i];
+            N = N % currency[i];
+        }
+    }
+    printf("smallest number of notes required: %d", count);
+    return 0;
+}
