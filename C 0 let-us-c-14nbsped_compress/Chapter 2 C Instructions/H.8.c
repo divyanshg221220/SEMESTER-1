e20060b81c395b0e0b4695c5530aafd54bd6bbbd
DIVYANSH GUPTA <divyanshg221220@outlook.com>
2026-02-17 09:40:01 +0000

    feat: Two numbers are input through the keyboard into two locations C and D. Write a program to interchange the contents of C and D.

---
+++ C 0 let-us-c-14nbsped_compress/Chapter 2 C Instructions/H.8.c
@@ -0,0 +1,16 @@
+// Two numbers are input through the keyboard into two locations C and D. Write a program to interchange the contents of C and D.
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    int C, D, temp;
+    printf("Enter C: ");
+    scanf("%d", &C);
+    printf("Enter D: ");
+    scanf("%d", &D);
+    temp = C;
+    C = D;
+    D = temp;
+    printf("C: %d\n", C);
+    printf("D: %d\n", D);
+    return 0;
+}
