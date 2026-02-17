96f1dc23095ab04cdf39913f40305bc071d534fd
DIVYANSH GUPTA <divyanshg221220@outlook.com>
2026-02-17 09:15:53 +0000

    feat: wind chill factor

---
+++ C 0 let-us-c-14nbsped_compress/Chapter 2 C Instructions/H.6.c
@@ -0,0 +1,16 @@
+// Wind chill factor is the felt air temperature on exposed skin due to wind. The wind chill temperature is always lower than the air temperature, and is calculated as per the following formula:
+// wcf = 35.74 + 0.6215t + ( 0.4275t - 35.75 ) * v ^ 0.16
+// where t is the temperature and v is the wind velocity. Write a program to receive values of t and v and calculate wind chill factor (wcf).
+#include <stdio.h>
+#include <math.h>
+int main(int argc, char const *argv[])
+{
+    float t, v, wcf;
+    printf("Enter the temperature in Fahrenheit: ");
+    scanf("%f", &t);
+    printf("Enter the wind velocity in miles per hour: ");
+    scanf("%f", &v);
+    wcf = 35.74 + 0.6215 * t + (0.4275 * t - 35.75) * pow(v, 0.16);
+    printf("wind chill factor: %.2f\n", wcf);
+    return 0;
+}
