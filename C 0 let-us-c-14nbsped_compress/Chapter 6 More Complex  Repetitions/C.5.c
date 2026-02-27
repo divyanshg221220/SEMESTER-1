347109bc13ef5ea981d6148698214aa6b79a68e3
divyanshg221220 <divyanshg221220@outlook.com>
2026-02-28 01:15:31 +0530

    fix

---
+++ C 0 let-us-c-14nbsped_compress/Chapter 6 More Complex  Repetitions/C.5.c
@@ -0,0 +1,29 @@
+// A machine is purchased which will produce earning of Rs. 1000 per year while it lasts. The machine costs Rs. 6000 and will have a salvage value of Rs. 2000 when it is condemned. If 9 percent per annum can be earned on alternate investments, write a program to determine what will be the minimum life of the machine to make it a more attractive investment compared to alternative investment? 
+// (c) Write a program to print the multiplication table of the number entered by the user. The table should get displayed in the following form: 
+// 29 * 1 = 29 
+// 29 * 2 = 58 
+// …
+#include <stdio.h>
+#include <math.h>
+int main(int argc, char const *argv[])
+{
+    float annual_earning = 1000.00, cost = 6000.00, salvage = 2000.00, rate = 0.12, present_worth_machine, present_worth_alternate;
+    int life = 1;
+    while (1)
+    {
+        present_worth_machine = -cost + (annual_earning * ((1 - pow(1 + rate, -life)) / rate)) + (salvage / pow(1 + rate, life));
+        present_worth_alternate = 0;
+        if (present_worth_machine >= present_worth_alternate)
+        {
+            printf("years: %d\n", life);
+            break;
+        }
+        life++;
+    }
+    for (int i = 1; i <= 10; i++)
+    {
+        printf("29 * %d = %d", i, 29 * i);
+        printf("\n");
+    }
+    return 0;
+}
---
+++ C 0 let-us-c-14nbsped_compress/Chapter 6 More Complex  Repetitions/C.8.c
@@ -0,0 +1,18 @@
+// The natural logarithm can be approximated by the following series.
+// (x - 1) / x + ( 1 / 2 ) ( (x - 1) / x ) ^ 2 + ( 1 / 2 ) ( (x - 1) / x ) ^ 3 + ( 1 / 2 ) ( (x - 1) / x ) ^ 4 + ....
+// If x is input through the keyboard, write a program to calculate the sum of first seven terms of this series. 
+#include <stdio.h>
+#include <math.h>
+int main(int argc, char const *argv[])
+{
+    float x, sum = 0.0;
+    printf("Enter the value of x: ");
+    scanf("%f", &x);
+    sum += (x - 1.0) / x;
+    for (int i = 2.0; i <= 7.0; i++)
+    {
+        sum += (1.0 / 2.0) * pow((x - 1.0) / x, i);
+    }
+    printf("sum: %.2f\n", sum);
+    return 0;
+}
