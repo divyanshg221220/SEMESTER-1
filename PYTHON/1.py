019f729fe318d9923b20a1dc4cb801116f27ae2e
divyanshg221220 <divyanshg221220@outlook.com>
2026-05-17 22:22:57 +0530

    .

--- PYTHON/1.py
+++
@@ -1,2 +0,0 @@
-# WAP that recieve I/P from user and gives O/P as even or odd.
-print("Even" if int(input("Enter a number : "))%2==0 else "Odd")
--- PYTHON/4.py
+++
@@ -1,2 +0,0 @@
-# WAP that recieve I/P from user and calculate Area of Rectangle
-print("Area of Rectangle : ",int(input("Enter length : "))*int(input("Enter breadth : ")))
