a47b59004d057423aa80dd878af59e83d0137d91
divyanshg221220 <divyanshg221220@outlook.com>
2026-05-20 01:41:35 +0530

    // Write a program to multiply any two 3 x 3 matrices.

---
+++ C 0 let-us-c-14nbsped_compress/Chapter 14 Multidimensional Arrays/C.10.c
@@ -0,0 +1,45 @@
+// Write a program to multiply any two 3 x 3 matrices. 
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    int matrix1[3][3], matrix2[3][3], result[3][3];
+    printf("Enter elements of matrix 1:\n");
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            printf("Enter element [%d][%d]: ", i, j);
+            scanf("%d", &matrix1[i][j]);
+        }
+    }
+    printf("Enter elements of matrix 2:\n");
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            printf("Enter element [%d][%d]: ", i, j);
+            scanf("%d", &matrix2[i][j]);
+        }
+    }
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            result[i][j] = 0;
+            for (int k = 0; k < 3; k++)
+            {
+                result[i][j] += matrix1[i][k] * matrix2[k][j];
+            }
+        }
+    }
+    printf("result:\n");
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            printf("%d ", result[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
