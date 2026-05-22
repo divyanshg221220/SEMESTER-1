5c1f26a28bb74d9ea6a5c968ddb47e16d0d6273e
divyanshg221220 <divyanshg221220@outlook.com>
2026-05-22 16:52:23 +0530

    // Given an array p[ 5 ], write a function to shift it circularly left by two positions. Thus, if p[ 0 ] = 15, p[ 1 ]= 30, p[ 2 ] = 28, p[ 3 ]= 19 and p[ 4 ] = 61 then after the shift p[ 0 ] = 28, p[ 1 ] = 19, p[ 2 ] = 61, p[ 3 ] = 15 and p[ 4 ] = 30. Call this function for a (4 x 5 ) matrix and get its rows left shifted.

---
+++ C 0 let-us-c-14nbsped_compress/Chapter 14 Multidimensional Arrays/C.12.c
@@ -0,0 +1,28 @@
+// A 6 x 6 matrix is entered through the keyboard. Write a program to obtain the Determinant value of this matrix. 
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    int matrix[6][6];
+    for (int i = 0; i < 6; i++)
+    {
+        for (int j = 0; j < 6; j++)
+        {
+            printf("Enter element [%d][%d]: ", i + 1, j + 1);
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+    int det = 0;
+    for (int i = 0; i < 6; i++)
+    {
+        int sign = (i % 2 == 0) ? 1 : -1;
+        int temp = matrix[0][i];
+        for (int j = 1; j < 6; j++)
+        {
+            int k = (j + i) % 6;
+            temp *= matrix[j][k];
+        }
+        det += sign * temp;
+    }
+    printf("determinant: %d\n", det);
+    return 0;
+}
