06cc8209cb5c6a4cdcb221e2ac6a546b45f2d645
divyanshg221220 <divyanshg221220@outlook.com>
2026-05-31 14:08:26 +0530

    // There is a structure called employee that holds information like employee code, name and date of joining. Write a program to create an array of structures and enter some data into it. Then ask the user to enter current date. Display the names of those employees whose tenure is greater than equal to 3 years.

--- C 0 let-us-c-14nbsped_compress/Chapter 17 Structures/D.5.c
+++ C 0 let-us-c-14nbsped_compress/Chapter 17 Structures/D.5.c
@@ -1,4 +1,4 @@
-// There is a structure called employee that holds information like employee code, name, date of joining. Write a program to create an array of the structure and enter some data into it. Then ask the user to enter current date. Display the names of those employees whose tenure is 3 or more than 3 years according to the given current date. 
+// There is a structure called employee that holds information like employee code, name and date of joining. Write a program to create an array of structures and enter some data into it. Then ask the user to enter current date. Display the names of those employees whose tenure is greater than equal to 3 years. 
 #include <stdio.h>
 struct date_of_joining
 {
