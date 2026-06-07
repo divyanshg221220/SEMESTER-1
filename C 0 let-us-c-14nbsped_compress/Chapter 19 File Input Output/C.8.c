3f70b8a35d8d2603af2614fc0cfecf3d878e2ff4
divyanshg221220 <divyanshg221220@outlook.com>
2026-06-07 23:35:28 +0530

    fix

--- C 0 let-us-c-14nbsped_compress/Chapter 19 File Input Output/C.8.c
+++ C 0 let-us-c-14nbsped_compress/Chapter 19 File Input Output/C.8.c
@@ -1,18 +1,18 @@
 // In the file ‘CUSTOMER.DAT’ there are 100 records with the following structure: 
-// struct customer 
-// {
+// struct  customer 
+// { 
 //     int  accno ; 
-//     char name[30] ; 
+//     char  name[ 30 ] ; 
 //     float  balance ; 
 // } ; 
 // In another file ‘TRANSACTIONS.DAT’ there are several records with the following structure: 
-// struct trans 
+// struct  trans 
 // { 
-//     int  accno ,  
-//     char trans_type ;
-//     float amount ; 
-// } ;
-// The parameter trans_type contains D/W indicating deposit or withdrawal of amount. Write a program to update ‘CUSTOMER.DAT’ file, i.e. if the trans_type is ‘D’ then update the balance of ‘CUSTOMER.DAT’ by adding amount to balance for the corresponding accno. Similarly, if trans_type is ‘W’ then subtract the amount from balance. However, while subtracting the amount make sure that the amount should not get overdrawn, i.e. at least 100 Rs. Should remain in the account.
+//     int  accno ; 
+//     char  trans_type ; 
+//     float  amount ; 
+// } ; 
+// The element trans_type contains D/W indicating deposit or withdrawal of amount. Write a program to update ‘CUSTOMER.DAT’ file, i.e., if the trans_type is ‘D’ then update the balance of ‘CUSTOMER.DAT’ by adding amount to balance for the corresponding accno. Similarly, if trans_type is ‘W’ then subtract the amount from balance. However, while subtracting the amount ensure that the amount should not get overdrawn, i.e., at least 100 Rs. Should remain in the account. 
 #include <stdio.h>
 struct customer
 {
