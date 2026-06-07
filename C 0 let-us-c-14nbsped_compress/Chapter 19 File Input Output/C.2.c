5bb36bd9ed9772f30145ea2ef1ace7bb76556dbe
divyanshg221220 <divyanshg221220@outlook.com>
2026-06-07 16:11:34 +0530

    // Write a program to append the contents of one file at the end of another.

---
+++ C 0 let-us-c-14nbsped_compress/Chapter 19 File Input Output/C.2.c
@@ -0,0 +1,28 @@
+// Write a program to append the contents of one file at the end of another.
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    FILE *fptr1, *fptr2;
+    char fname1[10], fname2[10];
+    printf("Enter file name 1: ");
+    scanf("%s", fname1);
+    printf("Enter file name 2: ");
+    scanf("%s", fname2);
+    fptr1 = fopen(fname1, "r");
+    fptr2 = fopen(fname2, "a");
+    if (fptr1 == NULL || fptr2 == NULL)
+    {
+        printf("error opening file");
+    }
+    else
+    {
+        char c;
+        while ((c = fgetc(fptr1)) != EOF)
+        {
+            fputc(c, fptr2);
+        }
+        fclose(fptr1);
+        fclose(fptr2);
+    }
+    return 0;
+}
