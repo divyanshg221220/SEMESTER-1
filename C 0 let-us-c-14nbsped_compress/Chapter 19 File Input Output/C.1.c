1001282bc3b11cc6b18aee797d9e51e8b00eb6c1
divyanshg221220 <divyanshg221220@outlook.com>
2026-06-07 16:07:26 +0530

    // Write a program to read a file and display its contents along with line numbers before each line.

---
+++ C 0 let-us-c-14nbsped_compress/Chapter 19 File Input Output/C.1.c
@@ -0,0 +1,37 @@
+// Write a program to read a file and display its contents along with line numbers before each line. 
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    FILE *fptr;
+    char fname[10];
+    printf("Enter file name: ");
+    scanf("%s", fname);
+    fptr = fopen(fname, "r");
+    if (fptr == NULL)
+    {
+        printf("error opening file");
+    }
+    else
+    {
+        int i = 1;
+        char c;
+        while ((c = fgetc(fptr)) != EOF)
+        {
+            if (i == 1 || c == '\n')
+            {
+                if (c == '\n')
+                {
+                    printf("\n");
+                }
+                printf("%d. ", i);
+                i++;
+            }
+            else
+            {
+                printf("%c", c);
+            }
+        }
+        fclose(fptr);
+    }
+    return 0;
+}
