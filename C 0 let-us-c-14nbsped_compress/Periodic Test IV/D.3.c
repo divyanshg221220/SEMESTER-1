76baa537c7401a0445dd58290b091531a6941b6a
divyanshg221220 <divyanshg221220@outlook.com>
2026-06-11 01:48:56 +0530

    // Write a program that receives a set of numbers as command- line arguments and prints their average.

--- C 0 let-us-c-14nbsped_compress/Periodic Test IV/D.3.c
+++ C 0 let-us-c-14nbsped_compress/Periodic Test IV/D.3.c
@@ -1,10 +1,14 @@
 // Write a program that receives a set of numbers as command- line arguments and prints their average. 
 #include <stdio.h>
+#include <stdlib.h>
 int main(int argc, char const *argv[])
 {
-    int sum = 0;
+    float sum = 0;
     for (int i = 1; i < argc; i++)
     {
         sum += atoi(argv[i]);
     }
-    printf("average: %.2f", )
+    float average = sum / (argc - 1);
+    printf("average: %.2f", average);
+    return 0;
+}
