39615524c81b7a96fa3cbaf6dcf63d92d131b3f1
divyanshg221220 <divyanshg221220@outlook.com>
2026-06-11 01:31:31 +0530

    // Write a program to multiply two integers using bitwise operators.

--- C 0 let-us-c-14nbsped_compress/Periodic Test IV/D.1.c
+++ C 0 let-us-c-14nbsped_compress/Periodic Test IV/D.1.c
@@ -1,1 +1,22 @@
 // Write a program to multiply two integers using bitwise operators. 
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    int a, b;
+    printf("Enter first number: ");
+    scanf("%d", &a);
+    printf("Enter second number: ");
+    scanf("%d", &b);
+    int result = 0;
+    while (b > 0)
+    {
+        if (b & 1)
+        {
+            result += a;
+        }
+        a <<= 1;
+        b >>= 1;
+    }
+    printf("result: %d", result);
+    return 0;
+}
