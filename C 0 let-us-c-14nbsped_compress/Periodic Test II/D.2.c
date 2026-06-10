38f7a26a6bc11ed652dcf95221127e5e676d4aa9
divyanshg221220 <divyanshg221220@outlook.com>
2026-06-10 20:28:45 +0530

    // Write a recursive function which prints the prime factors of the number that it receives when called from main( ).

--- C 0 let-us-c-14nbsped_compress/Periodic Test II/D.2.c
+++ C 0 let-us-c-14nbsped_compress/Periodic Test II/D.2.c
@@ -1,1 +1,27 @@
 // Write a recursive function which prints the prime factors of the number that it receives when called from main( ). 
+#include <stdio.h>
+void prime_factors(int num, int i)
+{
+    if (num <= 1)
+    {
+        return;
+    }
+    if (num % i == 0)
+    {
+        printf("%d ", i);
+        prime_factors(num / i, i);
+    }
+    else
+    {
+        prime_factors(num, i + 1);
+    }
+}
+int main(int argc, char const *argv[])
+{
+    int num;
+    printf("Enter a positive integer: ");
+    scanf("%d", &num);
+    printf("prime factors: ");
+    prime_factors(num, 2);
+    return 0;
+}
